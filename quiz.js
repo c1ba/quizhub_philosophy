2e5d75b4a82ca01ea8a0fc8bfa92ea24a2ab1e03
Alexandra Costache <ciba@users.noreply.github.com>
2021-05-18 17:01:21 +0300

    Random Commit

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 class question{
-    constructor(score){
+    constructor(score, categ){
         this.score = score;
+        this.categ = categ;
     }
 }
 
@@ -10,20 +11,20 @@ class category {
         this.score=0;
     }
 
-    insertQuestion(score){
-        let q = new question(score);
+    insertQuestion(score, categ){
+        let q = new question(score, categ);
         this.questionSeries.push(q);
     }
 
     addResult(n){
-       this.score += parseInt(this.questionSeries[n].score);
+       this.score += parseInt(n);
     }
 }
 
 
 
 let s = [],
-      results=["Nihilist", "Existentialist", "Hedonist"], cevaTest;
+      cts=["Nihilist", "Existentialist", "Hedonist", "Stoicist"], cevaTest;
 let c= new category, i=0, nihilism = new category, existentialism = new category, hedonism = new category, categList = [], categIndex = 0;
 let question_section = document.getElementById("question_section"), nextButton = document.getElementById("next_button"), rate_butts = document.getElementsByClassName("ans"), percentage_box = document.getElementById("percentage_box");
 
@@ -45,16 +46,21 @@ fetch("questions.json")
                     break;
                 }
             }
+            console.log(data[i].category);
+            console.log(cts);
+            console.log(categList);
             //Verifies if there was any checked value found. If not, it opens a pop-up. If yes, proceeds with the questions.
             if(checked_val!=undefined){
-                if(i != 0){
-                    if(i % 5 == 0){
-                        categIndex++;
-                        i = 0;
+                for(let k in cts){
+                    console.log(k);
+                    console.log(categList[k]);
+                    console.log(Boolean(data[i].category == cts[k]));
+                    if(data[i].category == cts[k]){
+                        categList[k].insertQuestion(rate_butts[checked_val].value, data[i].category);
+                        categList[k].addResult(rate_butts[checked_val].value);
+                        break;
                     }
                 }
-                categList[categIndex].insertQuestion(rate_butts[checked_val].value);
-                categList[categIndex].addResult(i);
                 i++;
                 if(i < data.length){
                 question_section.innerHTML = "<h2 style='text-align: center;'>"+data[i].question+"</h2>";
@@ -66,20 +72,21 @@ fetch("questions.json")
     
             }
             //When the array gets to undefined(the end of the quiz), it ends the quiz and shows the result.
-            if(categIndex == categList.length - 1 && i == 5){
+            if(i >= data.length){
                 question_section.innerHTML = "<h1>Fin.</h1>";
                 let resultRateNihilism = categList[0].score, resultRateExistentialism = categList[1].score, resultRateHedonism = categList[2].score, resultRoaster1, resultRoasterWinner, result_index;
                 let nihilistPercentage = (resultRateNihilism*100)/20, existentialistPercentage = (resultRateExistentialism*100)/20, hedonistPercentage = (resultRateHedonism*100)/20;
+
                 resultRateNihilism > resultRateExistentialism ? resultRoaster1 = resultRateNihilism : resultRoaster1 = resultRateExistentialism;
                 resultRateHedonism > resultRoaster1 ? resultRoasterWinner = resultRateHedonism : resultRoasterWinner = resultRoaster1;
+
                 for(k = 0; k < categList.length; k++){
-                    console.log("Score Per Category: " + categList[k].score);
-                    console.log("Final Score: "+ resultRoasterWinner);
                     if(categList[k].score == resultRoasterWinner) result_index = k;
                 }
-                console.log(result_index);
-                result_box.innerHTML = "<h2>Score: "+ results[result_index] + "</h2>";
+
+                result_box.innerHTML = "<h2>Score: "+ cts[result_index] + "</h2>";
                 percentage_box.innerHTML =
+                "<h3>How much you have from each:</h3>" +
                 "<h3>Nihilist: "+ nihilistPercentage +"%</h3>"+
                 "<h3>Existentialist: "+ existentialistPercentage +"%</h3>"+
                 "<h3>Hedonist: "+ hedonistPercentage +"%</h3>";
